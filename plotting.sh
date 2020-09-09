020f5e36d6c55ed4bf7e5eb2e6e07f6d79695c3e
Anna Paola Carrieri <acarrieri@uk.ibm.com>
2020-09-09 17:52:36 +0100

    added the plotting.sh file that is identical to train_models.sh but it calls plotting.py. This script does not do the training of the models in the json, it only produces the plots assuming the models in the json have been already trained

---
+++ plotting.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+if test -z "$1"; then
+  echo "Missing the name of the configuration file to use"
+  exit 1
+fi
+
+. ./common.sh &&
+docker run \
+  --rm \
+  -ti \
+  -u ${USER_ID}:${GROUP_ID} \
+  -v "${PWD}"/configs:/configs \
+  -v "${PWD}"/data:/data \
+  -v "${PWD}"/experiments:/experiments \
+  ${NAME}:${VERSION} \
+    python plotting.py -c /configs/"$1"
