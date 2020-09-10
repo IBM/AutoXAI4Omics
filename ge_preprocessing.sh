62aec7d3606820cf780b47f2ffbd30b01fd963a6
Anna Paola Carrieri <acarrieri@uk.ibm.com>
2020-09-10 14:34:48 +0100

    added some gene expression data files in /data and created a nre .sh file for testing Laura's code

---
+++ ge_preprocessing.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+if test -z "$1"; then
+  echo "Missing the name of the configuration file to use"
+  exit 1
+fi
+
+. ./common.sh &&
+docker run \
+  --rm \
+  -ti \
+  -u ${USER_ID} \
+  -v "${PWD}"/configs:/configs \
+  -v "${PWD}"/data:/data \
+  -v "${PWD}"/experiments:/experiments \
+  ${NAME}:${VERSION} \
+    python AoT_gene_expression_pre_processing.py --expressionfile /data/"$1" --expressiontype OTHER --Filtersamples 100 --Filtergenes 0 1 --output Processed_GE_L1000_discrete_drug_perturbation_blood_urea_nitrogen.csv
