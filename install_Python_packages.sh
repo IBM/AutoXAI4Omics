e57f364815762225ae4245927b77b2b128e061f6
Panagiotis Hadjidoukas <hat@zurich.ibm.com>
2021-02-26 00:14:30 +0100

    install_Python_packages.sh: sets autogluon version, reinstalls numpy

--- install_Python_packages.sh
+++ install_Python_packages.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
-pip install autogluon
+pip install autogluon==0.0.15
 pip install auto-sklearn==0.12.0
+pip uninstall -y numpy
+pip install numpy
 # pip install absl-py==0.10.0
 # pip install ConfigSpace==0.4.16
