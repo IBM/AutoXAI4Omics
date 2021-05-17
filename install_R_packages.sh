e6c56101eaa10586eb13ff7a9edcc195327e24cd
Niina Haiminen <nshaimin@us.ibm.com>
2021-05-17 10:24:41 -0400

    updated BiocManager version

--- install_R_packages.sh
+++ install_R_packages.sh
@@ -3,6 +3,6 @@
 R --no-save --silent <<-EOF
   if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager", ask = FALSE)
-	BiocManager::install(version = "3.11", ask = FALSE)
+	BiocManager::install(version = "3.12", ask = FALSE)
 	BiocManager::install("edgeR", ask = FALSE)
 EOF
