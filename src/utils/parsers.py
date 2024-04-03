0bcba74626940fcf35a0fa3f40ef652df005f950
James Strudwick <69587310+DrJStrudwick@users.noreply.github.com>
2024-04-03 12:18:40 +0100

    Post-release patches (#1)
    * Security: switched `os.mkdir` for `os.makedirs`
    
    * Fixed: copy-paste corrections
    
    * Fixed: redundant set casting
    
    * Changed: cleaned up docstring comment
    
    * Version bump
    
    * Removed config jsons that do not have paired data files
    
    * Changed: removed copyright notices from example json and data
    
    * Changed: Readme updated

--- src/utils/parsers.py
+++ src/utils/parsers.py
@@ -1,11 +1,11 @@
 # Copyright 2024 IBM Corp.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -374,9 +374,9 @@ def parse_MLSettings(problemEntry):
     }
     keys = set(problemEntry.keys())
 
-    if not set(keys).issubset(validKeys):
+    if not keys.issubset(validKeys):
         raise ValueError(
-            f"Invalid entry for problemEntry: {set(keys)-validKeys}. Valid options: {validKeys}"
+            f"Invalid entry for problemEntry: {keys-validKeys}. Valid options: {validKeys}"
         )
 
     if "seed_num" not in keys:
@@ -571,9 +571,9 @@ def parse_data(dataEntry):
     }
     keys = set(dataEntry.keys())
 
-    if not set(keys).issubset(validKeys):
+    if not keys.issubset(validKeys):
         raise ValueError(
-            f"Invalid entry for dataEntry: {set(keys)-validKeys}. Valid options: {validKeys}"
+            f"Invalid entry for dataEntry: {keys-validKeys}. Valid options: {validKeys}"
         )
 
     if (
@@ -658,9 +658,9 @@ def parse_microbiome(omicEntry):
     }
     keys = set(omicEntry.keys())
 
-    if not set(keys).issubset(validKeys):
+    if not keys.issubset(validKeys):
         raise ValueError(
-            f"Invalid entry for dataEntry: {set(keys)-validKeys}. Valid options: {validKeys}"
+            f"Invalid entry for dataEntry: {keys-validKeys}. Valid options: {validKeys}"
         )
 
     if "collapse_tax" not in keys:
@@ -760,9 +760,9 @@ def parse_geneExpression(omicEntry):
     }
     keys = set(omicEntry.keys())
 
-    if not set(keys).issubset(validKeys):
+    if not keys.issubset(validKeys):
         raise ValueError(
-            f"Invalid entry for dataEntry: {set(keys)-validKeys}. Valid options: {validKeys}"
+            f"Invalid entry for dataEntry: {keys-validKeys}. Valid options: {validKeys}"
         )
 
     if "expression_type" not in keys:
@@ -803,7 +803,7 @@ def parse_geneExpression(omicEntry):
                 raise ValueError(
                     "filter_genes must be a list of 2 integers greater than 0."
                 )
-            if (omicEntry["filter_genes"][0] < 0) or (omicEntry["filter_genes"][0] < 0):
+            if (omicEntry["filter_genes"][0] < 0) or (omicEntry["filter_genes"][1] < 0):
                 raise ValueError(
                     "filter_genes must be a list of 2 integers greater than 0."
                 )
@@ -832,9 +832,9 @@ def parse_metabolomic(omicEntry):
     }
     keys = set(omicEntry.keys())
 
-    if not set(keys).issubset(validKeys):
+    if not keys.issubset(validKeys):
         raise ValueError(
-            f"Invalid entry for dataEntry: {set(keys)-validKeys}. Valid options: {validKeys}"
+            f"Invalid entry for dataEntry: {keys-validKeys}. Valid options: {validKeys}"
         )
 
     if "filter_metabolomic_sample" not in keys:
@@ -874,7 +874,7 @@ def parse_metabolomic(omicEntry):
                     "filter_measurements must be a list of 2 integers greater than 0."
                 )
             if (omicEntry["filter_measurements"][0] < 0) or (
-                omicEntry["filter_measurements"][0] < 0
+                omicEntry["filter_measurements"][1] < 0
             ):
                 raise ValueError(
                     "filter_measurements must be a list of 2 integers greater than 0."
@@ -904,9 +904,9 @@ def parse_tabular(omicEntry):
     }
     keys = set(omicEntry.keys())
 
-    if not set(keys).issubset(validKeys):
+    if not keys.issubset(validKeys):
         raise ValueError(
-            f"Invalid entry for dataEntry: {set(keys)-validKeys}. Valid options: {validKeys}"
+            f"Invalid entry for dataEntry: {keys-validKeys}. Valid options: {validKeys}"
         )
 
     if "filter_tabular_sample" not in keys:
@@ -952,7 +952,7 @@ def parse_tabular(omicEntry):
                     "filter_tabular_measurements must be a list of 2 integers greater than 0."
                 )
             if (omicEntry["filter_tabular_measurements"][0] < 0) or (
-                omicEntry["filter_tabular_measurements"][0] < 0
+                omicEntry["filter_tabular_measurements"][1] < 0
             ):
                 raise ValueError(
                     "filter_tabular_measurements must be a list of 2 integers greater than 0."
@@ -1001,9 +1001,9 @@ def parse_prediction(predictionEntry):
     validKeys = {"file_path", "outfile_name", "metadata_file"}
     keys = set(predictionEntry.keys())
 
-    if not set(keys).issubset(validKeys):
+    if not keys.issubset(validKeys):
         raise ValueError(
-            f"Invalid entry for predictionEntry: {set(keys)-validKeys}. Valid options: {validKeys}"
+            f"Invalid entry for predictionEntry: {keys-validKeys}. Valid options: {validKeys}"
         )
 
     if (
@@ -1204,12 +1204,14 @@ def parse_FS_settings(problem_type, FS_dict):
             "min_features": 10,
             "max_features": None,
             "interval": 1,
-            "eval_model": "RandomForestClassifier"
-            if problem_type == CLASSIFICATION
-            else "RandomForestRegressor",
-            "eval_metric": "f1_score"
-            if problem_type == CLASSIFICATION
-            else "mean_squared_error",
+            "eval_model": (
+                "RandomForestClassifier"
+                if problem_type == CLASSIFICATION
+                else "RandomForestRegressor"
+            ),
+            "eval_metric": (
+                "f1_score" if problem_type == CLASSIFICATION else "mean_squared_error"
+            ),
             "low": problem_type != CLASSIFICATION,
         }
 
